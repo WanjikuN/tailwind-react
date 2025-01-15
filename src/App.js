b652770ac4547cce9b6c092ebb75c239d5ffdd06
pwanjiku <cikunjoroge4@gmail.com>
2025-01-15 10:57:02 +0300

    feat: Add responsive designs

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="flex justify-center items-center h-screen bg-blue-500">
-        <h1 className="text-4xl text-sta font-bold">Tailwind CSS!</h1>
-      </div>
+     <div class="max-w-md mx-auto bg-white rounded-[112px] shadow-md overflow-hidden md:max-w-2xl">
+  <div class="md:flex">
+    <div class="md:shrink-0">
+      <img class="h-100 w-full object-cover md:h-full md:w-24" src="./logo192.png" alt="Modern building architecture" />
+    </div>
+    <div class="p-8">
+      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
+      <p class="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
+    </div>
+  </div>
+</div>
     </div>
   );
 }
